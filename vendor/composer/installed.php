039b55955b6a9d20c59075557df1ed58f3d6a177
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-09-07 13:44:48 -0300

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'martin-fields/glass-moduleinstaller',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'martin-fields/glass-moduleinstaller' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
